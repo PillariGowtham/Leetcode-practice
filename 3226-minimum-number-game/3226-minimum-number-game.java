afe904a42d8dddf927c4435b9a11515c1db02cd0
Pillari Gowtham <142710385+PillariGowtham@users.noreply.github.com>
2025-07-21 10:30:34 +0530

    Time: 2 ms (97.82%), Space: 44.8 MB (79.37%) - LeetHub

---
+++ 3226-minimum-number-game/3226-minimum-number-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        for(int i=0;i<n;i+=2){
+            int temp=nums[i];
+            if(i+1<n){
+                nums[i]=nums[i+1];
+                nums[i+1]=temp;
+            }
+        }
+        return nums;
+    }
+}
