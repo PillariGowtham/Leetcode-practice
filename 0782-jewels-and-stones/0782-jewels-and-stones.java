bffe9c1055e02e6643918c0c2c083e090c1f8a4f
Pillari Gowtham <142710385+PillariGowtham@users.noreply.github.com>
2025-07-21 10:43:42 +0530

    Time: 1 ms (57.38%), Space: 42.1 MB (14.92%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        for(int i=0;i<stones.length();i++){
+            char c=stones.charAt(i);
+            if(jewels.contains(c+"")){
+                count++;
+            }
+        }
+        return count;
+    }
+}
