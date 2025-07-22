259eca0e56694520ed81ff53300fbacf1b22731a
Pillari Gowtham <142710385+PillariGowtham@users.noreply.github.com>
2025-07-22 14:33:31 +0530

    Time: 1 ms (100%), Space: 45.5 MB (79.54%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        HashSet<Integer> h=new HashSet<>();
-        int[] result=new int[2];
-        int n=grid.length,k=0;
-        int[] arr=new int[n*n];
+        int n=grid.length;
+        boolean[] arr=new boolean[n*n];
+        int[] res=new int[2];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                arr[k++]=grid[i][j];
+                if(arr[grid[i][j]-1]){
+                    res[0]=grid[i][j];
+                }
+                arr[grid[i][j]-1]=true;
             }
         }
-        Arrays.sort(arr);
         for(int i=0;i<n*n;i++){
-            if(h.contains(arr[i])){
-                result[0]=arr[i];
-            }
-            h.add(arr[i]);
-            if(!h.contains(i+1)){
-                result[1]=i+1;
+            if(!arr[i]){
+                res[1]=i+1;
             }
         }
-        return result;
+        return res;
     }
 }
