e22d0b24acb56f8be257ab40020093e0a190a8bb
Pillari Gowtham <142710385+PillariGowtham@users.noreply.github.com>
2025-08-09 19:38:47 +0530

    Time: 0 ms (100%), Space: 41.3 MB (9.72%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,12 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        long x=1;
-        for(int i=1;x<=n;i++){
-            if(x==n){
-                return true;
-            }
-            x=(long)Math.pow(2,i);
-        }
-        return false;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
