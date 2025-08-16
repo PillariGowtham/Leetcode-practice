2da194367e68ff7c693260adfac069ca2a2262fb
Pillari Gowtham <142710385+PillariGowtham@users.noreply.github.com>
2025-08-16 22:44:25 +0530

    Time: 0 ms (100%), Space: 40.3 MB (94.76%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.java
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -1,5 +1,12 @@
 class Solution {
     public int maximum69Number (int num) {
-        return Integer.parseInt(Integer.toString(num).replaceFirst("6", "9"));
+        char c[]=Integer.toString(num).toCharArray();
+        for(int i=0;i<c.length;i++){
+            if(c[i]=='6'){
+                c[i]='9';
+                break;
+            }
+        }
+        return Integer.parseInt(new String(c));
     }
 }
