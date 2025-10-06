2230a96905e128d8a25eb9feec44aa744386be1a
Pillari Gowtham <142710385+PillariGowtham@users.noreply.github.com>
2025-10-06 10:47:24 +0530

    Time: 0 ms (100%), Space: 42.2 MB (18.38%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0; // carry over
+        }
+
+        // If all digits were 9, we need one extra digit
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+        return ans;
+    }
+}
